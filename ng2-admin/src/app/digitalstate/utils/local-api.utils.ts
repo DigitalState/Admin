703c64559294dd0ef3e561360020f3c80d852cfc
Bushra Addlan <bushra.addlan@gmail.com>
2017-08-18 12:49:50 -0400

    Created a Local API utility class and function to generate router links from entity URIs.

---
+++ ng2-admin/src/app/digitalstate/utils/local-api.utils.ts
@@ -0,0 +1,43 @@
+import isArray from 'lodash/isArray';
+
+export class LocalApiUtils {
+
+    /**
+     * Generates a Router Link to an entity's `show` component from a URI.
+     *
+     * @param uri
+     * @returns {{routerLink: Array, title: null}}
+     */
+    static createEntityLinkFromUri(uri: string): { routerLink: Array<string>, title: any } {
+        if (uri) {
+            const exploded = uri.split('/');
+
+            if (isArray(exploded) && exploded.length >= 2) {
+                let link = {
+                    routerLink: [],
+                    title: null,
+                };
+
+                const uuid = exploded[exploded.length - 1];
+                const entityPrefix = exploded[exploded.length - 2];
+
+                switch (entityPrefix) {
+                    case 'scenarios':
+                    case 'submissions':
+                        link.routerLink = ['/pages', 'services', entityPrefix, uuid, 'show'];
+                        break;
+                    case 'case-statuses':
+                        // @Todo Implement case statuses link by changing the route to the case-status component in a similar way to how scenarios are implemented
+                        alert('Case Statuses link is not implemented yet.');
+                        link.routerLink = ['/pages', 'cases', entityPrefix, uuid, 'show'];
+                        break;
+                    default:
+                        link.routerLink = ['/pages', entityPrefix, uuid, 'show'];
+                        break;
+                }
+
+                return link;
+            }
+        }
+    }
+}
