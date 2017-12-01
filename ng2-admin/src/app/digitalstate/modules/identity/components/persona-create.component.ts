3964f4b1a6ea025be2a54d85c444fe056aa43f3a
Bushra Addlan <bushra.addlan@gmail.com>
2017-12-01 16:32:12 -0500

    Fixed Persona back link.

--- ng2-admin/src/app/digitalstate/modules/identity/components/persona-create.component.ts
+++ ng2-admin/src/app/digitalstate/modules/identity/components/persona-create.component.ts
@@ -1,8 +1,5 @@
 import { Component, Injector } from '@angular/core';
-import { NgForm, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
-
-import { CustomValidators } from 'ng2-validation';
 
 import { MicroserviceConfig } from '../../../../shared/providers/microservice.provider';
 import { IdentityUtils } from '../../../../shared/utils/identity.utils';
@@ -10,6 +7,7 @@ import { IdentityUtils } from '../../../../shared/utils/identity.utils';
 import { Link } from '../../../models/link';
 import { EntityApiService } from '../entity-api.service';
 import { DsBaseEntityFormComponent } from '../../../components/base-entity-form.component';
+import { DsPersonaFormComponent } from './persona-form.component';
 
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
@@ -18,7 +16,7 @@ import { Observable } from 'rxjs/Observable';
     selector: 'ds-persona-create',
     templateUrl: '../templates/persona-form.template.html'
 })
-export class DsPersonaCreateComponent extends DsBaseEntityFormComponent {
+export class DsPersonaCreateComponent extends DsPersonaFormComponent {
 
     entityParentUrlParam = 'identityUuid';
     headerTitle = 'ds.microservices.entity.types.persona';
@@ -32,8 +30,7 @@ export class DsPersonaCreateComponent extends DsBaseEntityFormComponent {
                 microserviceConfig: MicroserviceConfig,
                 entityApiService: EntityApiService) {
 
-        super(injector, microserviceConfig);
-        this.entityApiService = entityApiService;
+        super(injector, microserviceConfig, entityApiService);
     }
 
     ngOnInit() {
